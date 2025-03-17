4e64c14290f2750844bf88378bc91250e9a599fc
Thomas van Bruggen <tbruggen@gmail.com>
2025-03-17 09:27:26 +0100

    removed config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append('.')
-
-from importers.comdirect import ComdirectImporter
-
-CONFIG = [ComdirectImporter('Assets:Comdirect:Gemeinschaftskonto', 'DE17200411110853623700')]
-
-
